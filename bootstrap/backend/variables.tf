1a9110543a70736722a910a4d2661571815809c5
Moni040 <monish.hv212212@gmail.com>
2026-06-28 14:30:43 +0000

    resource creation completed

---
+++ bootstrap/backend/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "cloudscale-terraform-state-monish-2026"
+}
+
+variable "lock_table_name" {
+  default = "cloudscale-terraform-locks"
+}
